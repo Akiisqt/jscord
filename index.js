c9e812cecb55e782aaa2267e3b6bd6ea5526c4d7
Chanator <71682867+Aki-9470@users.noreply.github.com>
2021-02-11 23:46:11 -0600

    hello

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const Discord = require("./jscord/index.js"), fetch = require("node-fetch"), { constants } = require("./jscord/src/constants/consts.js");
+const Discord = require("./jscord/index.js"), fetch = require("node-fetch"), { constants } = require("./jscord/src/constants/consts.js"), config = require("./config.json");
 const client = new Discord.Client();
 
 client.on("ready", () => {
@@ -6,16 +6,32 @@ client.on("ready", () => {
 });
 
 client.on("message", async (message) => {
+    if (message.author.bot) return;
+
     console.log(message.author.username + " (" + message.author.id + "): " + message.content);
     if (message.content == "!ping") {
         await createMessage("Pong", message.channel_id);
     }
 });
 
-async function createMessage(content, channelId) {
+function makeid(length) {
+    let result = '';
+    let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return result;
+}
+
+async function createMessage(content, channelId, nonce) {
+    if (!nonce) {
+        nonce = makeid(12);
+    }
+
     const data = {
         "content": content,
-        "tts": false
+        "tts": false,
+        nonce
     };
 
     const headers = { "Content-Type": "application/json", "Authorization": `Bot ${client.token}` };
@@ -26,8 +42,17 @@ async function createMessage(content, channelId) {
         body: JSON.stringify(data),
     });
 
-    const json = await response.json();
-    console.log(json);
+    if (response.status === 429) {
+        const wait = parseInt(response.headers.get("Retry-After")) * 1000;
+        console.log(`Rate limit hit! Waiting ${wait}ms. (raw: ${(wait / 1000)})`);
+        setTimeout(function () {
+            createMessage(content, channelId);
+        }, wait);
+    } else {
+        response.json().then((msg) => {
+            console.log(`MessageCreate: ${msg.id}`);
+        });
+    }
 }
 
-client.login("token");
+client.login(config.token);
